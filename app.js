d8e73e3d2527b77a29ecebd7e78cd301292ca73b
Maram Hani Mohammad <maramhanisbaih@gmail.com>
2021-03-07 17:00:55 +0200

    solve pro 4

--- app.js
+++ app.js
@@ -87,15 +87,20 @@ IMPORTANT DETAIL: You may not use the arithmetic operator + in this function. To
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSumArray() function and see if the test passes.*/
 
 // Write your code here
-var testArray = [2, 3, 4]; //eslint-disable-line
+let testArray = [2, 3, 4]; //eslint-disable-line
 
 function sumArray(sumArr) { //eslint-disable-line
-
+let resourceful = []
+for (let i = 0; i < sumArr.length; i++){
+let sr = sum(sum(sumArr[i], sumArr[i+1])[0], sumArr[i+2])[0];
+resourceful.push(sr);
 }
-
+let arra = +testArray+ 'was passed in as an array of numbers'+resourceful+ 'is their sum';
+return arra
+}
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray(testArray);
+testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
